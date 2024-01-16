d7d22ac7f921c47c8f82c7e39437766a1706e5a1
Zero Systems <isszero06@gmail.com>
2024-01-16 18:54:45 +0200

    Update stock.py

--- zero_no_stock_backorder/models/stock.py
+++ zero_no_stock_backorder/models/stock.py
@@ -20,20 +20,20 @@ class StockPicking(models.Model):
     product_uom_qty = fields.Float(
         'Total Qty Demand',digits='Product Unit of Measure',compute='_quantity_picking_compute')
 
-    quantity_done = fields.Float(
+    quantity = fields.Float(
         'Total Quantity Done',digits='Product Unit of Measure', compute='_quantity_picking_compute')
 
 
-    @api.onchange("move_ids_without_package","move_ids_without_package.product_uom_qty", "move_ids_without_package.quantity_done")
+    @api.onchange("move_ids_without_package","move_ids_without_package.product_uom_qty", "move_ids_without_package.quantity")
     def _quantity_picking_compute(self):
         for order in self:
             uom_qty = 0.0
             done_qty = 0.0
             for line in order.move_ids_without_package:
                 uom_qty += (line.product_uom_qty)
-                done_qty += (line.quantity_done)
+                done_qty += (line.quantity)
             order.product_uom_qty = uom_qty
-            order.quantity_done = done_qty
+            order.quantity = done_qty
 
   
 
@@ -44,13 +44,13 @@ class StockPicking(models.Model):
             user_receipt = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('zero_no_stock_backorder.zero_no_stock_backorder_receipt').id)])
             user_internal = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('zero_no_stock_backorder.zero_no_stock_backorder_internal').id)])
             if not user_delivery and not move.origin and move.picking_type_id.code =='outgoing':
-                if move.product_uom_qty != move.quantity_done:
+                if move.product_uom_qty != move.quantity:
                     raise UserError(_('Total Qty Demand Not Equal Total Quantity Done'))
             if not user_receipt and not move.origin and move.picking_type_id.code =='incoming':
-                if move.product_uom_qty != move.quantity_done:
+                if move.product_uom_qty != move.quantity:
                     raise UserError(_('Total Qty Demand Not Equal Total Quantity Done'))
             if not user_internal and not move.origin and move.picking_type_id.code =='internal':
-                if move.product_uom_qty != move.quantity_done:
+                if move.product_uom_qty != move.quantity:
                     raise UserError(_('Total Qty Demand Not Equal Total Quantity Done'))
 
         return res
